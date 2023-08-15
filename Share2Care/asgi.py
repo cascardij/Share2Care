ba9804a08c11a0643e0d38d628e0eda24a59bae5
João Cascardi <joao.cascardi@aluno.ifsp.edu.br>
2023-08-15 10:56:50 -0300

    criação de projeto/app

---
+++ Share2Care/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Share2Care project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Share2Care.settings')
+
+application = get_asgi_application()
