1bdd2cd68b3ff82f65578292d72c3c360f604361
João Cascardi <joao.cascardi@aluno.ifsp.edu.br>
2023-08-16 09:30:26 -0300

    rotas/templates

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("sobre", views.index, name="sobre"),
+    path("equipe", views.index, name="equipe")
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render 
 
+def index (request):
+    return render(request, 'app/index.html')
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'app/sobre.html')
+
+def index (request):
+    return render (request, 'app/equipe.html')
